daee16260dce60cff6eb65d9f53bef4be8ad1176
pawarkSmita11 <smitakpawar122@gmail.com>
2021-03-09 20:07:21 +0530

    Updating Keyword class

---
+++ src/main/java/com/sp/uikeywords/Keyword.java
@@ -0,0 +1,18 @@
+package com.sp.uikeywords;
+
+public class Keyword {
+public void openBrowser(String browser) {
+	switch (browser) {
+	case "chrome":
+		
+		break;
+	case "Firefox":
+		break;
+	case "IE":
+		break;
+	default:
+		break;
+	}
+
+}
+}
